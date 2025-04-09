fe7d7f07f053676b5fddc5ec3cb87ab08b85e03e
Diablo777x77 <nanopool69@gmail.com>
2025-04-09 21:28:58 +0200

    Add files via upload

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="container">
+      <h1>Dashboard</h1>
+      <p>Tu znajdziesz swoje cele i motywację.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
